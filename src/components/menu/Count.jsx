48586b043e521eac55338f25313c67777bdf96a4
BravoDario <adriandariobravo@gmail.com>
2024-12-13 14:02:47 -0600

    blog/cv

--- src/components/menu/Count.jsx
+++ src/components/menu/Count.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react"
 
-export function Count({num}){
-   
+export function Count({ num }) {
+
     const [sumar, SetSumar] = useState(num)
 
-    const multiplicar =() =>{
-       return SetSumar(sumar+1)
+    const multiplicar = () => {
+        return SetSumar(sumar + 1)
     }
 
-    return(
+    return (
         <>
             <button onClick={multiplicar} className="bg-gray-900 text-white rounded-lg p-2">
                 sumar
