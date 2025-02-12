90ede75c602570dce941b6c1dac58bd0ff9e6734
BravoDario <adriandariobravo@gmail.com>
2025-02-12 15:45:06 -0600

    4astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
-
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-
-  adapter: node({
-    mode: 'standalone'
-  }),
+  integrations: [react(), tailwind()]
 });
