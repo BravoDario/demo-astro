42547458283b6753a07acb6cb80a40651c0083a5
BravoDario <adriandariobravo@gmail.com>
2025-02-13 11:32:30 -0600

    chore: Update dependencies and add registration form to MyAusita page

--- src/components/myausita/Validador.jsx
+++ src/components/myausita/Validador.jsx
@@ -16,14 +16,49 @@ const Validador = () => {
         return password.length >= 8;
     };
 
+    const handler = async () => {
+        const formData = new FormData();
+        formData.append("name", "Darío Bravo");
+        formData.append("email", "dario.bravo@gmail.com");
+        formData.append("password", "123456789")
+
+        try {
+            const response = await fetch("apis/register", {
+                params: "sí hay",
+                method: "POST"
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error("Error:", error);
+
+        };
+    }
+
+
     return (
         <div>
-            <input
-                type="password"
-                value={password}
-                onChange={handlePasswordChange}
-            />
-            {isValid ? <p>Contraseña válida</p> : <p>Contraseña inválida</p>}
+            <h1>Register</h1>
+            <p>Already have an account? <a href="/signin">Sign in</a></p>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+            <label htmlFor="email" >Email</label>
+            <input type="email" name="email" id="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+
+                onClick={handler}
+            >
+                Register
+            </button>
         </div>
     );
 };
