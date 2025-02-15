87e0e6c57646d4a35e90963878cb29b017dab6fa
Dario Bravo <adriandariobravo@gmail.com>
2025-02-14 18:22:41 -0600

    new Images

--- src/components/myausita/MyauImages.jsx
+++ src/components/myausita/MyauImages.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import ImagesCarrousel from "./ImagesCarrousel.jsx";
 //importar ícono close de font-awesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
-const MyauImages = ({ }) => {
+const MyauImages = ({}) => {
     const token = localStorage.getItem("token");
     console.log(token);
     const [isLogin, setIsLogin] = useState(token ? true : false);
@@ -12,53 +12,62 @@ const MyauImages = ({ }) => {
 
     useEffect(() => {
         setIsLogin(token ? true : false);
-        !isLogin ? window.location.href = "/" : '';
+        !isLogin ? (window.location.href = "/") : "";
     }, [token]);
 
     const handleOpenModal = (img) => {
         setImg(img);
-    }
+    };
 
     const handleCLoseModal = () => {
         setImg(null);
-    }
+    };
 
-    return isLogin ?
-        <div className='relative'>
-            <section
-                class="bg-gradient-to-r from-purple-500 to-pink-500 text-white h-full -mt-5 pt-12"
-            >
+    return isLogin ? (
+        <div className="relative pb-[4.5rem]">
+            <section class="bg-gradient-to-r from-purple-500 to-pink-500 text-white -mt-5 pt-12 h-full">
                 <h2 class="text-3xl text-center font-bold uppercase mb-2 font-sans">
-                Myau Page ;3
+                    Myau Page ;3
                 </h2>
                 <div class="w-2/3 mx-auto bg-slate-200 p-8 rounded-md">
-                    <ImagesCarrousel handleCLoseModal={handleCLoseModal} handleOpenModal={handleOpenModal} client:only="react" />
+                    <ImagesCarrousel
+                        handleCLoseModal={handleCLoseModal}
+                        handleOpenModal={handleOpenModal}
+                        client:only="react"
+                    />
                 </div>
             </section>
-            {
-                img ?
-                    <div className='absolute top-0 left-0 w-full h-full m-auto z-20 bg-black bg-opacity-50 flex justify-center items-center'>
-                        <div className='relative w-1/2 bg-white h-fit rounded-md p-4 flex flex-row justify-center items-start'>
-                            <button
-                                className='absolute top-2 right-2 p-2 bg-red-500 text-white rounded-md'
-                                onClick={handleCLoseModal}
-                            >
-                                <FontAwesomeIcon icon={faCoffee} />
-                            </button>
-                            <div className="w-80 h-80 filter grayscale blur-md">
-                                <img src={img.src} className='w-full h-full filter grayscale blur-md' alt="" />
-                            </div>
-                            <div className='w-2/3 h-fit align-top '>
-                                <h2 className='text-center font-bold uppercase'>{img.title}</h2>
-                                <p>
-                                    {img.description}
-                                </p>
-                            </div>
+            {img ? (
+                <div className="absolute top-0 left-0 w-full h-full m-auto z-20 bg-black bg-opacity-50 flex justify-center items-center">
+                    <div className="relative w-1/2 bg-white text-slate-900 h-fit rounded-md p-4 flex flex-row gap-2 justify-center items-start">
+                        <button
+                            className="absolute top-2 right-2 p-2 bg-red-500 text-white rounded-md"
+                            onClick={handleCLoseModal}
+                        >
+                            <FontAwesomeIcon icon={faCoffee} />
+                        </button>
+                        <div className="w-80 h-fit overflow-hidden">
+                            <img
+                                src={img.src}
+                                className="w-full h-auto min-h-40 object-cover object-top"
+                                alt=""
+                            />
                         </div>
-                    </div> : ''
-            }
+                        <div className="w-2/3 h-fit mt-4">
+                            <h2 className="text-center font-bold uppercase">
+                                {img.title}
+                            </h2>
+                            <p>{img.description}</p>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                ""
+            )}
         </div>
-        : '';
-}
+    ) : (
+        ""
+    );
+};
 
 export default MyauImages;
--- src/data/images.js
+++ src/data/images.js
@@ -3,139 +3,127 @@ const images = [
         src: "/pictures/image (1).jpg",
         alt: "",
         title: "Myausitos",
-        description: "Me gusta mucho ;3"
+        description: "Saludos cariño, probablemente veas esto. Así que te darás cuenta de las posibilidades que hay en este sitio."
     },
     {
         src: "/pictures/image (2).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Myausitos digitales",
+        description: "Ahora podrás ver los dibujitos que has hecho durante nuestro tiempo que nos hemos conocido. Como dato: Me gustan mucho los dibujitos que haces pq siempre te quedan muy bonitos. :3"
     },
     {
         src: "/pictures/image (3).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Myausitos dormidos",
+        description: "Las fotitos mostradas suelen tener una descripción, donde hablan de qué es lo que me gustan. Por ejemplo, esta son los myausitos durmiendo en un estilo chibi. Estilo originario de japón que muestran elementos de una manera que hace sentir ternura en el observador" 
     },
     {
         src: "/pictures/image (4).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Myausita",
+        description: "Ñam ;3"
     },
     {
         src: "/pictures/image (5).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Myausitos análogos",
+        description: "Un dibujito hecho a mano por Dayra. Realista porque muestra los sentimientos de la pareja. Pero no tanto porque no tienen piernas. Aún así es un gran dibujo."
     },
     {
         src: "/pictures/image (6).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Para la myausita:",
+        description: "Seguramente Darío debió decir a la autora: 'También te quiero'."
     },
     {
         src: "/pictures/image (7).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Myausita x2",
+        description: "Ñam ;3"
     },
     {
         src: "/pictures/image (8).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Ñam ",
+        description: "Myausita"
     },
     {
         src: "/pictures/image (9).jpg",
         alt: "",
-        title: "",
-        description: ""
-    },
-    {
-        src: "/pictures/image (10).jpg",
-        alt: "",
-        title: "",
-        description: ""
-    },
-    {
-        src: "/pictures/image (11).jpg",
-        alt: "",
-        title: "",
-        description: ""
+        title: "De nuevo, otra vez repetido",
+        description: "Myausita: Ñam ;3" 
     },
     {
         src: "/pictures/image (12).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Chile morrón",
+        description: "Cualquier imagen de ese señor me recuerda a la myausita. GRRRR. Pero bueno, es muy tierno y es un gran reflejo de la myausita"
     },
     {
         src: "/pictures/image (13).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Myausitos anime",
+        description: "描画スタイルの大きな変化。ラテンアメリカ西部の伝統的なスタイルから日本の文化的参照スタイルに変わります。  モカちゃんが教えてくれた、色んな見た目になれるアプリなんだって、どれどれ"
     },
     {
         src: "/pictures/image (14).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "Myausita",
+        description: "Ya te la sabes. ;3"
     },
     {
         src: "/pictures/image (15).jpg",
         alt: "",
-        title: "",
-        description: ""
+        title: "E-Myausitos",
+        description: "Representación de los Myausitos durmiendo. Se trata de una simulación del comportamiento de los myausitos en un contexto donde habitan en conjunto."
     },
     {
         src: "/pictures/image (16).jpg",
         alt: "image (16)",
-        title: "",
-        description: ""
+        title: "Myuasito Peluche",
+        description: "El myausito siendo peluche siendo abrazado por la myausita"
     },
     {
         src: "/pictures/image (17).jpg",
         alt: "image (17)",
-        title: "",
-        description: ""
+        title: "Myausita peluche",
+        description: "La myausita siendo peluche siendo abrazada por el myausito"
     },
     {
         src: "/pictures/image (18).jpg",
         alt: "image (18)",
-        title: "",
-        description: ""
+        title: "Cerati si fuera realista",
+        description: "Un gran dibujo. Muestra la técnica avanzada del realismo aplicado por la autora. Es un retrato del famoso cantaautor argentino Gustavo Cerati."
     },
     {
         src: "/pictures/image (19).jpg",
         alt: "image (19)",
-        title: "",
-        description: ""
+        title: "Myausitos si fueran románticos",
+        description: "Grrr. Antojan"
     },
     {
         src: "/pictures/image (20).jpg",
         alt: "image (20)",
-        title: "",
-        description: ""
+        title: "Myausitos siendo, clarament, realistas",
+        description: "Representación gráfica de los myausitos existiendo en el plano existencial tridimensional. Proporciones y elementos de la vida diaria son evidentes en el gráfico."
     },
     {
         src: "/pictures/image (21).jpg",
         alt: "image (21)",
-        title: "",
-        description: ""
+        title: "Myausitos solos",
+        description: "Acciones y sentimientos previos a acciones poco íntegras ante las energias de los myaus."
     },
     {
         src: "/pictures/image (22).jpg",
         alt: "image (22)",
-        title: "",
-        description: ""
+        title: "Sueño soñado",
+        description: "Dos myausitas, ñam"
     },
     {
         src: "/pictures/image (23).jpg",
         alt: "image (23)",
-        title: "",
-        description: ""
+        title: "Darío si supiera tocar guitarra",
+        description: "Representación de la presunta pareja de Dayra. Representación de darío tocando la guitarra. Dayra afirma: 'Viva AMLO'"
     },
     {
         src: "/pictures/image (24).jpg",
@@ -266,15 +254,51 @@ const images = [
     {
         src: "/pictures/image (7).png",
         alt: "image (7) in png",
-        title: "",
-        description: ""
+        title: "Ponchi Celu",
+        description: "Obra navideña que refleja el amor a la fauna felina, además de la muestra de la domesticación exitosa y el cariño que refleja ese ser vivo."
     },
     {
         src: "/pictures/image.gif",
         alt: "image (1) in gif",
-        title: "",
-        description: ""
+        title: "Myausitos comiendo el Ado",
+        description: "Obra maestra de la artista. Muestra un avance en la técnica en el manejor de las herramientas tecnológicas para la creación de elementos multimedia."
     },
+    {
+        src: "/pictures/1mes con el myaau.jpg",
+        alt: "image (10) in png",
+        title: "Myausitos primer mes",
+    },
+    {
+        src: "/pictures/4mesesuwu.png",
+        alt: "image (10) in png",
+        title: "Myausitos segundo mes",
+    },
+    {
+        src: "/pictures/5 meses.png",
+        alt: "image (10) in png",
+        title: "Myausitos tercer mes",
+    },
+    {
+        src: "/pictures/aqui casual con el myau.jpg",
+        alt: "image (10) in png",
+        title: "Myausitos cuarto mes",
+    },
+    {
+        src: "/pictures/darin myau.jpg",
+        alt: "image (10) in png",
+        title: "Myausitos quinto mes",
+    },
+    {
+        src: "/pictures/dario tqm.jpg",
+        alt: "image (10) in png",
+        title: "Myausitos sexto mes",
+    },
+    {
+        src: "/pictures/dibujo dosmeses uwu.jpg",
+        alt: "image (10) in png",
+        title: "Myausitos séptimo mes",
+    },
+
 ];
 
 export default images;
