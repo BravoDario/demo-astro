77ddab1b78f12d6aefde4b7a57cbf52ddc075544
BravoDario <adriandariobravo@gmail.com>
2025-02-26 18:00:30 -0600

    revisar :o

--- src/components/GraphLetters.jsx
+++ src/components/GraphLetters.jsx
@@ -1,58 +1,58 @@
 import { useEffect, useState } from "react";
 
+const indexes = {
+    a: { index: [0, 0], counter: 0 },
+    b: { index: [0, 1], counter: 0 },
+    c: { index: [1, 0], counter: 0 },
+    d: { index: [0, 2], counter: 0 },
+    e: { index: [1, 1], counter: 0 },
+    f: { index: [2, 0], counter: 0 },
+    g: { index: [0, 3], counter: 0 },
+    h: { index: [1, 2], counter: 0 },
+    i: { index: [2, 1], counter: 0 },
+    j: { index: [3, 0], counter: 0 },
+    k: { index: [0, 4], counter: 0 },
+    l: { index: [1, 3], counter: 0 },
+    m: { index: [2, 2], counter: 0 },
+    n: { index: [3, 1], counter: 0 },
+    "ñ": { index: [4, 0], counter: 0 },
+    o: { index: [0, 5], counter: 0 },
+    p: { index: [1, 4], counter: 0 },
+    q: { index: [2, 3], counter: 0 },
+    r: { index: [3, 2], counter: 0 },
+    s: { index: [4, 1], counter: 0 },
+    t: { index: [5, 0], counter: 0 },
+    u: { index: [0, 6], counter: 0 },
+    v: { index: [1, 5], counter: 0 },
+    w: { index: [2, 4], counter: 0 },
+    x: { index: [3, 3], counter: 0 },
+    y: { index: [4, 2], counter: 0 },
+    z: { index: [5, 1], counter: 0 },
+    1: { index: [10, 7], counter: 0 },
+    2: { index: [10, 8], counter: 0 },
+    3: { index: [10, 9], counter: 0 },
+    4: { index: [10, 10], counter: 0 },
+    5: { index: [10, 11], counter: 0 },
+    6: { index: [10, 12], counter: 0 },
+    7: { index: [10, 13], counter: 0 },
+    8: { index: [10, 14], counter: 0 },
+    9: { index: [10, 15], counter: 0 },
+    0: { index: [10, 16], counter: 0 },
+    " ": { index: [10, 6], counter: 0 }
+}
+
 const GraphLetters = () => {
     const [number, setNumber] = useState(30);
-    const [player, setPlayer] = useState([0, 0]);
-    const [target, setTarget] = useState([Math.floor(Math.random() * number), Math.floor(Math.random() * number)]);
-    const [word, setWord] = useState("abcdefghijklmnñopqrstuvwxyz1234567890");
+    const [word, setWord] = useState("text");
     const [letters, setLetters] = useState([]);
-
-    const letterIndex = {
-        a: [0, 0],
-        b: [0, 1],
-        c: [1, 0],
-        d: [1, 1],
-        e: [0, 2],
-        f: [2, 0],
-        g: [2, 2],
-        h: [0, 3],
-        i: [3, 0],
-        j: [3, 3],
-        k: [0, 4],
-        l: [4, 0],
-        m: [4, 4],
-        n: [0, 5],
-        "ñ": [5, 1],
-        o: [5, 0],
-        p: [5, 5],
-        q: [0, 6],
-        r: [6, 0],
-        s: [6, 6],
-        t: [0, 7],
-        u: [7, 0],
-        v: [7, 7],
-        w: [0, 8],
-        x: [8, 0],
-        y: [8, 8],
-        z: [0, 9],
-        1: [12, 12],
-        2: [12, 13],
-        3: [13, 12],
-        4: [13, 13],
-        5: [13, 14],
-        6: [14, 15],
-        7: [15, 16],
-        8: [16, 17],
-        9: [18, 19],
-        0: [19, 20],
-        " ": [20, 20],
-    }
+    const [letterIndex, setLetterIndex] = useState(indexes);
+    const [wordBreaker, setWordBreaker] = useState(word.split("").map(letter => letterIndex[letter.toLowerCase()] ?? ""));
 
     const getMesh = () => {
         let squareY = [];
         for (let i = 1; i <= number; i++) {
             let squareX = [];
-            for (let j = 1; j <= number; j++) {
+            for (let j = 0; j < number; j++) {
                 squareX.push(j);
             }
             squareY.push(squareX);
@@ -61,16 +61,65 @@ const GraphLetters = () => {
     };
 
     const breakWord = () => {
-        setLetters([...word.split("").map(letter => letterIndex[letter.toLowerCase()])]);
+        const currentWord = word.split("")
+
+        currentWord.map(letter => {
+            const letterSearch = letter?.toLowerCase();
+            const itemBreakWord = indexes[letterSearch];
+            const quantityLetter = word.split("").filter(l => l.toLowerCase() == letterSearch).length;
+
+            const newLetter = {
+                index: letterIndex[letterSearch].index,
+                counter: quantityLetter,
+            }
+
+            setWordBreaker({ ...wordBreaker, [letterSearch]: newLetter });
+        })
+
+        setWordBreaker(currentWord.map(letter => letterIndex[letter.toLowerCase()] ?? ""))
+
+        currentWord.forEach((letter, index) => {
+
+            const letterSearch = letter?.toLowerCase();
+            const quantityLetter = word.split("").filter(l => l.toLowerCase() == letterSearch).length;
+            const newLetter = {
+                index: letterIndex[letterSearch].index,
+                counter: quantityLetter,
+            }
+            //console.log({ ...letterIndex, [letterSearch]: newLetter });
+
+            //setLetterIndex();
+        })
+
+        setLetters([...currentWord.map(letter => {
+            const letterSearch = letter?.toLowerCase();
+            return letterIndex[letterSearch].index ?? ""
+        }
+        )]);
     }
 
     useEffect(() => {
         breakWord();
+        //console.log(letterIndex);
     }, [word]);
 
     const getIndexLetter = (indexX, indexY) => {
         const l = letters.filter(l => l[0] == indexX && l[1] == [indexY])
         return l.length > 0 ? true : false;
+    }
+
+    const getLetter = (x, y) => {
+        for (const [key, value] of Object.entries(letters)) {
+            if (value.index[0] === x && value.index[1] === y) {
+                return key;
+            }
+        }
+        return null;
+    }
+
+    const getCounterByLetter = (x, y) => {
+        const letter = getLetter(x, y);
+        return letterIndex[letter.toLowerCase()].counter;
     }
 
     return (
@@ -82,11 +131,11 @@ const GraphLetters = () => {
                     return (
                         <>
                             <div className="flex flex-row justify-end">
-                                {indexY == 0 ? ["Y/X", ...square].map((square) => 
+                                {indexY == 0 ? ["Y/X", ...square].map((square) =>
                                     <span className=" min-h-[25px] min-w-[25px] border-[1px] text-center" key={square}>
                                         {square}
-                                    </span>) 
-                                : ''}
+                                    </span>)
+                                    : ''}
                             </div>
 
                             <div className="flex flex-row items-center justify-end" key={indexY}>
@@ -94,8 +143,9 @@ const GraphLetters = () => {
 
                                 {
                                     square.map((square, indexX) => <span
-                                        className={`${getIndexLetter(indexX, indexY) ? "bg-red-300" : 'bg-slate-400'} min-h-[25px] min-w-[25px] border-[1px] hover:bg-slate-600 hover:text-white`}
+                                        className={`${getIndexLetter(indexX, indexY) ? "bg-red-300" : 'bg-slate-400'} min-h-[25px] min-w-[25px] border-[1px] hover:bg-slate-600 hover:text-white text-center`}
                                         key={indexX}>
+                                        {/* getCounterByLetter(indexX, indexY) */}
                                     </span>)
                                 }
                             </div>
