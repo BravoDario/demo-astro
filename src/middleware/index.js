7b6ff1584f667bfac1db01483502bdbc25677e1a
BravoDario <adriandariobravo@gmail.com>
2025-04-02 11:05:33 -0600

    dfghjk

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,10 @@
-import Cookies from 'js-cookie';
+//import Cookies from 'js-cookie';
 import { defineMiddleware } from "astro/middleware";
-import { getToken } from '../utilities/tokenChecker';
+//import { getToken } from '../utilities/tokenChecker';
 
 export const onRequest = defineMiddleware((context, next) => {
-    console.log(getToken());
+    //console.log(getToken());
+    console.log("midelguere");
 
     return next();
 });
