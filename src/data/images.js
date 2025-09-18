885a1012a982b0c84a6114e6f3c4fcb1960fb6e0
Dario Bravo <adriandariobravo@gmail.com>
2025-09-18 14:23:37 -0600

    refactorización para assets

--- src/data/images.js
+++ src/data/images.js
@@ -1,300 +1,300 @@
 const images = [
     {
-        src: "/pictures/image (1).jpg",
+        src: "/pictures/aniversario/image (1).jpg",
         alt: "",
         title: "Myausitos",
         description: "Saludos cariño, probablemente veas esto. Así que te darás cuenta de las posibilidades que hay en este sitio."
     },
     {
-        src: "/pictures/image (2).jpg",
+        src: "/pictures/aniversario/image (2).jpg",
         alt: "",
         title: "Myausitos digitales",
         description: "Ahora podrás ver los dibujitos que has hecho durante nuestro tiempo que nos hemos conocido. Como dato: Me gustan mucho los dibujitos que haces pq siempre te quedan muy bonitos. :3"
     },
     {
-        src: "/pictures/image (3).jpg",
+        src: "/pictures/aniversario/image (3).jpg",
         alt: "",
         title: "Myausitos dormidos",
         description: "Las fotitos mostradas suelen tener una descripción, donde hablan de qué es lo que me gustan. Por ejemplo, esta son los myausitos durmiendo en un estilo chibi. Estilo originario de japón que muestran elementos de una manera que hace sentir ternura en el observador" 
     },
     {
-        src: "/pictures/image (4).jpg",
+        src: "/pictures/aniversario/image (4).jpg",
         alt: "",
         title: "Myausita",
         description: "Ñam ;3"
     },
     {
-        src: "/pictures/image (5).jpg",
+        src: "/pictures/aniversario/image (5).jpg",
         alt: "",
         title: "Myausitos análogos",
         description: "Un dibujito hecho a mano por Dayra. Realista porque muestra los sentimientos de la pareja. Pero no tanto porque no tienen piernas. Aún así es un gran dibujo."
     },
     {
-        src: "/pictures/image (6).jpg",
+        src: "/pictures/aniversario/image (6).jpg",
         alt: "",
         title: "Para la myausita:",
         description: "Seguramente Darío debió decir a la autora: 'También te quiero'."
     },
     {
-        src: "/pictures/image (7).jpg",
+        src: "/pictures/aniversario/image (7).jpg",
         alt: "",
         title: "Myausita x2",
         description: "Ñam ;3"
     },
     {
-        src: "/pictures/image (8).jpg",
+        src: "/pictures/aniversario/image (8).jpg",
         alt: "",
         title: "Ñam ",
         description: "Myausita"
     },
     {
-        src: "/pictures/image (9).jpg",
+        src: "/pictures/aniversario/image (9).jpg",
         alt: "",
         title: "De nuevo, otra vez repetido",
         description: "Myausita: Ñam ;3" 
     },
     {
-        src: "/pictures/image (12).jpg",
+        src: "/pictures/aniversario/image (12).jpg",
         alt: "",
         title: "Chile morrón",
         description: "Cualquier imagen de ese señor me recuerda a la myausita. GRRRR. Pero bueno, es muy tierno y es un gran reflejo de la myausita"
     },
     {
-        src: "/pictures/image (13).jpg",
+        src: "/pictures/aniversario/image (13).jpg",
         alt: "",
         title: "Myausitos anime",
         description: "描画スタイルの大きな変化。ラテンアメリカ西部の伝統的なスタイルから日本の文化的参照スタイルに変わります。  モカちゃんが教えてくれた、色んな見た目になれるアプリなんだって、どれどれ"
     },
     {
-        src: "/pictures/image (14).jpg",
+        src: "/pictures/aniversario/image (14).jpg",
         alt: "",
         title: "Myausita",
         description: "Ya te la sabes. ;3"
     },
     {
-        src: "/pictures/image (15).jpg",
+        src: "/pictures/aniversario/image (15).jpg",
         alt: "",
         title: "E-Myausitos",
         description: "Representación de los Myausitos durmiendo. Se trata de una simulación del comportamiento de los myausitos en un contexto donde habitan en conjunto."
     },
     {
-        src: "/pictures/image (16).jpg",
+        src: "/pictures/aniversario/image (16).jpg",
         alt: "image (16)",
         title: "Myuasito Peluche",
         description: "El myausito siendo peluche siendo abrazado por la myausita"
     },
     {
-        src: "/pictures/image (17).jpg",
+        src: "/pictures/aniversario/image (17).jpg",
         alt: "image (17)",
         title: "Myausita peluche",
         description: "La myausita siendo peluche siendo abrazada por el myausito"
     },
     {
-        src: "/pictures/image (18).jpg",
+        src: "/pictures/aniversario/image (18).jpg",
         alt: "image (18)",
         title: "Cerati si fuera realista",
         description: "Un gran dibujo. Muestra la técnica avanzada del realismo aplicado por la autora. Es un retrato del famoso cantaautor argentino Gustavo Cerati."
     },
     {
-        src: "/pictures/image (19).jpg",
+        src: "/pictures/aniversario/image (19).jpg",
         alt: "image (19)",
         title: "Myausitos si fueran románticos",
         description: "Grrr. Antojan"
     },
     {
-        src: "/pictures/image (20).jpg",
+        src: "/pictures/aniversario/image (20).jpg",
         alt: "image (20)",
         title: "Myausitos siendo, clarament, realistas",
         description: "Representación gráfica de los myausitos existiendo en el plano existencial tridimensional. Proporciones y elementos de la vida diaria son evidentes en el gráfico."
     },
     {
-        src: "/pictures/image (21).jpg",
+        src: "/pictures/aniversario/image (21).jpg",
         alt: "image (21)",
         title: "Myausitos solos",
         description: "Acciones y sentimientos previos a acciones poco íntegras ante las energias de los myaus."
     },
     {
-        src: "/pictures/image (22).jpg",
+        src: "/pictures/aniversario/image (22).jpg",
         alt: "image (22)",
         title: "Sueño soñado",
         description: "Dos myausitas, ñam"
     },
     {
-        src: "/pictures/image (23).jpg",
+        src: "/pictures/aniversario/image (23).jpg",
         alt: "image (23)",
         title: "Darío si supiera tocar guitarra",
         description: "Representación de la presunta pareja de Dayra. Representación de darío tocando la guitarra. Dayra afirma: 'Viva AMLO'"
     },
     {
-        src: "/pictures/image (24).jpg",
+        src: "/pictures/aniversario/image (24).jpg",
         alt: "image (24)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (25).jpg",
+        src: "/pictures/aniversario/image (25).jpg",
         alt: "image (25)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (26).jpg",
+        src: "/pictures/aniversario/image (26).jpg",
         alt: "image (26)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (27).jpg",
+        src: "/pictures/aniversario/image (27).jpg",
         alt: "image (27)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (28).jpg",
+        src: "/pictures/aniversario/image (28).jpg",
         alt: "image (28)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (29).jpg",
+        src: "/pictures/aniversario/image (29).jpg",
         alt: "image (29)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (30).jpg",
+        src: "/pictures/aniversario/image (30).jpg",
         alt: "image (30)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (31).jpg",
+        src: "/pictures/aniversario/image (31).jpg",
         alt: "image (31)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (32).jpg",
+        src: "/pictures/aniversario/image (32).jpg",
         alt: "image (32)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (33).jpg",
+        src: "/pictures/aniversario/image (33).jpg",
         alt: "image (33)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (34).jpg",
+        src: "/pictures/aniversario/image (34).jpg",
         alt: "image (34)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (35).jpg",
+        src: "/pictures/aniversario/image (35).jpg",
         alt: "image (35)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (36).jpg",
+        src: "/pictures/aniversario/image (36).jpg",
         alt: "image (36)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (37).jpg",
+        src: "/pictures/aniversario/image (37).jpg",
         alt: "image (37)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (38).jpg",
+        src: "/pictures/aniversario/image (38).jpg",
         alt: "image (38)",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (1).png",
+        src: "/pictures/aniversario/image (1).png",
         alt: "image (1) in png",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (2).png",
+        src: "/pictures/aniversario/image (2).png",
         alt: "image (2) in png",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (3).png",
+        src: "/pictures/aniversario/image (3).png",
         alt: "image (3) in png",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (4).png",
+        src: "/pictures/aniversario/image (4).png",
         alt: "image (4) in png",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (5).png",
+        src: "/pictures/aniversario/image (5).png",
         alt: "image (5) in png",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (6).png",
+        src: "/pictures/aniversario/image (6).png",
         alt: "image (6) in png",
         title: "",
         description: ""
     },
     {
-        src: "/pictures/image (7).png",
+        src: "/pictures/aniversario/image (7).png",
         alt: "image (7) in png",
         title: "Ponchi Celu",
         description: "Obra navideña que refleja el amor a la fauna felina, además de la muestra de la domesticación exitosa y el cariño que refleja ese ser vivo."
     },
     {
-        src: "/pictures/image.gif",
+        src: "/pictures/aniversario/image.gif",
         alt: "image (1) in gif",
         title: "Myausitos comiendo el Ado",
         description: "Obra maestra de la artista. Muestra un avance en la técnica en el manejor de las herramientas tecnológicas para la creación de elementos multimedia."
     },
     {
-        src: "/pictures/1mes con el myaau.jpg",
+        src: "/pictures/aniversario/1mes con el myaau.jpg",
         alt: "image (10) in png",
         title: "Myausitos primer mes",
     },
     {
-        src: "/pictures/4mesesuwu.png",
+        src: "/pictures/aniversario/4mesesuwu.png",
         alt: "image (10) in png",
         title: "Myausitos segundo mes",
     },
     {
-        src: "/pictures/5 meses.png",
+        src: "/pictures/aniversario/5 meses.png",
         alt: "image (10) in png",
         title: "Myausitos tercer mes",
     },
     {
-        src: "/pictures/aqui casual con el myau.jpg",
+        src: "/pictures/aniversario/aqui casual con el myau.jpg",
         alt: "image (10) in png",
         title: "Myausitos cuarto mes",
     },
     {
-        src: "/pictures/darin myau.jpg",
+        src: "/pictures/aniversario/darin myau.jpg",
         alt: "image (10) in png",
         title: "Myausitos quinto mes",
     },
     {
-        src: "/pictures/dario tqm.jpg",
+        src: "/pictures/aniversario/dario tqm.jpg",
         alt: "image (10) in png",
         title: "Myausitos sexto mes",
     },
     {
-        src: "/pictures/dibujo dosmeses uwu.jpg",
+        src: "/pictures/aniversario/dibujo dosmeses uwu.jpg",
         alt: "image (10) in png",
         title: "Myausitos séptimo mes",
     },
